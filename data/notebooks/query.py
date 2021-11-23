f574ab1ce282debf25cab637a3edbfff3ca3b4e0
Amanda L Martin <hythloda@gmail.com>
2021-11-23 10:59:42 -0500

    reorder query commands

--- data/notebooks/query.py
+++ data/notebooks/query.py
@@ -4,6 +4,17 @@ aggs = quotes.by(caf.AggCombo(caf.AggMin("Ask_Min=AskPrice"),caf.AggAvg("AskSize
 
 relatedQuotes = trades.aj(quotes, "Symbol, Time = BidTime", "BidTime, BidPrice, BidSize")
 
+from deephaven.MovingAverages import ByEmaSimple
+
+ema_price_10min = ByEmaSimple('BD_SKIP','BD_SKIP','TIME',10,'MINUTES', type='LEVEL')
+ema_price_60min =  ByEmaSimple('BD_SKIP','BD_SKIP','TIME',60,'MINUTES', type='LEVEL')
+ema_price_100ticks = ByEmaSimple('BD_SKIP','BD_SKIP','TICK',10,None, type='LEVEL')
+
+withEmas = trades.update(\
+    "EmaMin10 = ema_price_10min.update(Time, Price, Symbol)",\
+    "EmaMin60 = ema_price_60min.update(Time, Price, Symbol)",\
+    "EmaTick100 = ema_price_100ticks.update(Time, Price, Symbol)")
+
 from deephaven import Plot
 
 plotOHLC = Plot.ohlcPlot("AXP", candle.where("Symbol=`AXP`"), "Time", "Open", "High", "Low", "Close")\
@@ -15,14 +26,3 @@ vWapPlot = Plot.plot("VWAP",candle.where("Symbol=`AAPL`"),"Time","VWap")\
 
 plotSingle = Plot.plot("AAPL", trades.where("Symbol = `AAPL`"), "Time", "Price")\
     .show()
-
-from deephaven.MovingAverages import ByEmaSimple
-
-ema_price_10min = ByEmaSimple('BD_SKIP','BD_SKIP','TIME',10,'MINUTES', type='LEVEL')
-ema_price_60min =  ByEmaSimple('BD_SKIP','BD_SKIP','TIME',60,'MINUTES', type='LEVEL')
-ema_price_100ticks = ByEmaSimple('BD_SKIP','BD_SKIP','TICK',10,None, type='LEVEL')
-
-withEmas = trades.update(\
-    "EmaMin10 = ema_price_10min.update(Time, Price, Symbol)",\
-    "EmaMin60 = ema_price_60min.update(Time, Price, Symbol)",\
-    "EmaTick100 = ema_price_100ticks.update(Time, Price, Symbol)")
