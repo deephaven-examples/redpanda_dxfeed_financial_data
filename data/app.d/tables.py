5a2b736dd395b1c1b43d84207b57d706c76124f8
JJ Brosnan <84038776+jjbrosnan@users.noreply.github.com>
2025-01-31 08:29:46 -0500

    Remove all int_ instances

--- data/app.d/tables.py
+++ data/app.d/tables.py
@@ -21,35 +21,35 @@ trades = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'Trade', key_spec=
 
 quotes = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'Quote', key_spec=KeyValueSpec.IGNORE, value_spec=ck.json_spec([
     ('Symbol', dht.string),
-    ('Sequence',   dht.int_),
+    ('Sequence',   dht.int64),
     ('Timestamp',  dht.Instant),
     ('BidTime',   dht.Instant),
     ('BidExchangeCode', dht.string),
     ('BidPrice',   dht.double),
-    ('BidSize',  dht.int_),
+    ('BidSize',  dht.int64),
     ('AskTime',    dht.Instant),
     ('AskExchangeCode',  dht.string),
     ('AskPrice',    dht.double),
-    ('AskSize',    dht.int_),
-    ('Scope',    dht.int_)
+    ('AskSize',    dht.int64),
+    ('Scope',    dht.int64)
     ]),table_type = TableType.append()).sort_descending(["KafkaOffset"])
 
 
 candle = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'Candle', key_spec=KeyValueSpec.IGNORE, value_spec=ck.json_spec([
     ('Symbol', dht.string),
-    ('EventFlags',   dht.int_),
+    ('EventFlags',   dht.int64),
     ('Index',  dht.int64),
     ('Timestamp',   dht.Instant),
-    ('Sequence', dht.int_),
-    ('Count',   dht.int_),
+    ('Sequence', dht.int64),
+    ('Count',   dht.int64),
     ('Open',  dht.double),
     ('High',    dht.double),
     ('Low',  dht.double),
     ('Close',    dht.double),
-    ('Volume',    dht.int_),
+    ('Volume',    dht.int64),
     ('VWap',    dht.double),
-    ('BidVolume',  dht.int_),
-    ('AskVolume',    dht.int_),
+    ('BidVolume',  dht.int64),
+    ('AskVolume',    dht.int64),
     ('OpenInterest',    dht.string),
     ('ImpVolatility',    dht.string)
     ]),table_type = TableType.Append).sort_descending(["KafkaOffset"])
@@ -60,52 +60,52 @@ profile = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'Profile', key_sp
     ('Symbol', dht.string),
     ('Beta',   dht.double),
     ('EPS',  dht.double),
-    ('DivFreq',   dht.int_),
+    ('DivFreq',   dht.int64),
     ('ExdDivAmount', dht.double),
-    ('ExdDivDate',   dht.int_),
+    ('ExdDivDate',   dht.int64),
     ('52HighPrice',  dht.double),
     ('52LowPrice',    dht.double),
-    ('Shares',  dht.int_),
+    ('Shares',  dht.int64),
     ('FreeFloat',    dht.string),
     ('HighLimitPrice',    dht.double),
     ('LowLimitPrice',    dht.double),
     ('HaltStartTime',  dht.Instant),
     ('HaltEndTime',    dht.Instant),
     ('Description',    dht.string),
-    ('RawFlags',    dht.int_),
+    ('RawFlags',    dht.int64),
     ('StatusReason',    dht.string),
-    ('TradingStatus',    dht.int_),
-    ('ShortSaleRestriction',    dht.int_)
+    ('TradingStatus',    dht.int64),
+    ('ShortSaleRestriction',    dht.int64)
     ]),table_type = TableType.append()).sort_descending(["KafkaOffset"])
 
 
 summary = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'Summary', key_spec=KeyValueSpec.IGNORE, value_spec=ck.json_spec([
     ('Symbol',    dht.string),
-    ('DayId',    dht.int_),
+    ('DayId',    dht.int64),
     ('DayOpenPrice',    dht.double),
     ('DayHighPrice',    dht.double),
     ('DayLowPrice',    dht.double),
     ('DayClosePrice',    dht.double),
-    ('PrevDayId',    dht.int_),
+    ('PrevDayId',    dht.int64),
     ('PrevDayClosePrice',    dht.double),
-    ('PrevDayVolume',    dht.int_),
-    ('OpenInterest',    dht.int_),
-    ('RawFlags',    dht.int_),
+    ('PrevDayVolume',    dht.int64),
+    ('OpenInterest',    dht.int64),
+    ('RawFlags',    dht.int64),
     ('ExchangeCode',    dht.string),
-    ('DayClosePriceType',    dht.int_),
-    ('PrevDayClosePriceType',    dht.int_),
-    ('Scope',    dht.int_)
+    ('DayClosePriceType',    dht.int64),
+    ('PrevDayClosePriceType',    dht.int64),
+    ('Scope',    dht.int64)
     ]),table_type = TableType.append()).sort_descending(["KafkaOffset"])
 
 order = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'Order', key_spec=KeyValueSpec.IGNORE, value_spec=ck.json_spec([
     ('Symbol',    dht.string),
-    ('EventFlags',    dht.int_),
+    ('EventFlags',    dht.int64),
     ('Index',    dht.int64),
     ('Timestamp',    dht.Instant),
-    ('Sequence',    dht.int_),
+    ('Sequence',    dht.int64),
     ('Price',    dht.double),
-    ('Size',    dht.int_),
-    ('Count',    dht.int_),
+    ('Size',    dht.int64),
+    ('Count',    dht.int64),
     ('Scope',    dht.string),
     ('Side',    dht.string),
     ('ExchangeCode',    dht.string),
@@ -124,7 +124,7 @@ underlying = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'Underlying', 
 
 timeAndSale = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'TimeAndSale', key_spec=KeyValueSpec.IGNORE, value_spec=ck.json_spec([
     ('Symbol',    dht.string),
-    ('EventFlags',    dht.int_),
+    ('EventFlags',    dht.int64),
     ('Index',    dht.int64),
     ('Timestamp',    dht.Instant),
     ('ExchangeCode',    dht.string),
@@ -133,26 +133,26 @@ timeAndSale = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'TimeAndSale'
     ('BidPrice',    dht.double),
     ('AskPrice',    dht.double),
     ('ExchangeSaleConditions',    dht.string),
-    ('RawFlags',    dht.int_),
+    ('RawFlags',    dht.int64),
     ('Buyer',    dht.string),
     ('Seller',    dht.string),
-    ('Side',    dht.int_),
-    ('Type',    dht.int_),
-    ('IsValidTick',    dht.int_),
-    ('IsEthTrade',    dht.int_),
-    ('TradeThroughExempt',    dht.int_),
-    ('IsSpreadLeg',    dht.int_),
-    ('Scope',    dht.int_)
+    ('Side',    dht.int64),
+    ('Type',    dht.int64),
+    ('IsValidTick',    dht.int64),
+    ('IsEthTrade',    dht.int64),
+    ('TradeThroughExempt',    dht.int64),
+    ('IsSpreadLeg',    dht.int64),
+    ('Scope',    dht.int64)
     ]),table_type = TableType.append()).sort_descending(["KafkaOffset"])
 
 
 series = ck.consume({'bootstrap.servers': 'redpanda:29092'} , 'Series', key_spec=KeyValueSpec.IGNORE, value_spec=ck.json_spec([
     ('Symbol',    dht.string),
-    ('EventFlags',    dht.int_),
+    ('EventFlags',    dht.int64),
     ('Index',    dht.int64),
     ('Timestamp',    dht.Instant),
-    ('Sequence',    dht.int_),
-    ('Expiration',    dht.int_),
+    ('Sequence',    dht.int64),
+    ('Expiration',    dht.int64),
     ('Volatility',    dht.double),
     ('PutCallRatio',    dht.double),
     ('ForwardPrice',    dht.double),
